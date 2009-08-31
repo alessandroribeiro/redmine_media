02be1b43af8c3c6aacf5097840b616f6f4c2f20b
root <root@vm-integritas-systems.(none)>
2009-08-31 00:10:14 -0300

    471: resolved

--- app/models/media_file.rb
+++ app/models/media_file.rb
@@ -90,6 +90,67 @@ class MediaFile < ActiveRecord::Base
     return public_filename
   end
 
+  def can_be_edited_by? other_user
+    if user==other_user
+      return true
+    end
+    role = other_user.role_for_project project 
+    if role.name=="Manager" || role.name=="Admin"
+      return true
+    end
+    false
+  end
+
+  def time_past
+    diff_in_seconds = Time.now - created_on
+    if (diff_in_seconds<60) 
+      if diff_in_seconds==1
+        return [diff_in_seconds,"second"]
+      end
+      return [diff_in_seconds,"seconds"]
+    end
+    
+    diff_in_minutes = diff_in_seconds/60    
+    if (diff_in_minutes<60)
+      if diff_in_minutes==1
+        return [diff_in_minutes,"minute"]
+      end
+      return [diff_in_minutes,"minutes"]
+    end
+    
+    diff_in_hours = diff_in_minutes/60
+    if (diff_in_hours<60)
+      if diff_in_hours==1
+        return [diff_in_hours,"hour"]
+      end      
+      return [diff_in_hours,"hours"]
+    end
+    
+    diff_in_days = diff_in_hours/24
+    if (diff_in_days<30)
+      if diff_in_days==1
+        return [diff_in_days,"day"]
+      end      
+      return [diff_in_days,"days"]
+    end
+    
+    diff_in_months = diff_in_days/30
+    if (diff_in_months<12)
+      if diff_in_months==1
+        return [diff_in_months,"month"]
+      end      
+      return [diff_in_months,"months"]
+    end
+    
+    diff_in_years = diff_in_months/12
+    if diff_in_years==1
+      return [diff_in_years,"year"]
+    end
+
+    return [diff_in_years,"years"]
+    
+  end
+
   protected
 
   # This method creates the ffmpeg command that we'll be using
